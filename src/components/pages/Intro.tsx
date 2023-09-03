4d3f9f48fe49d0350be6e87d5c3ac66d031474dd
callmejeje <choije401@naver.com>
2023-09-03 21:05:09 +0900

    [#5] feat: 닉네임 유효성 검사, 둘러보기, 이미 생성한 홈피 존재 시 이동

--- src/components/pages/Intro.tsx
+++ src/components/pages/Intro.tsx
@@ -1,6 +1,7 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router';
 import { styled } from 'styled-components';
+import { getRandomHomep } from '../../apis/services/homepService';
 import logo from '../../assets/logo.svg';
 import Button from '../atoms/Button';
 import Input from '../atoms/Input';
@@ -8,12 +9,37 @@ import Input from '../atoms/Input';
 const Intro = () => {
   const navigate = useNavigate();
   const [nickname, setNickname] = useState('');
+
+  const validateNickname = () => {
+    if (nickname.length < 1 || nickname.length > 12) {
+      throw new Error('닉네임은 최대 12자까지 입력 가능합니다.');
+    }
+  };
+
   const saveNickname = () => {
+    try {
+      validateNickname();
+    } catch (e) {
+      if (e instanceof Error) alert(e.message);
+      return;
+    }
+
     localStorage.setItem('nickname', nickname);
     navigate('/create');
   };
 
-  const lookAround = () => {};
+  const lookAround = () => {
+    getRandomHomep().then((data) => {
+      navigate(`/profile/${data.uuid}`);
+    });
+  };
+
+  useEffect(() => {
+    const uuid = localStorage.getItem('uuid');
+    if (uuid) {
+      navigate(`/profile/${uuid}`);
+    }
+  }, [navigate]);
 
   return (
     <IntroWrapper>
@@ -31,7 +57,7 @@ const Intro = () => {
           fill={'fill'}
           size={'full'}
           disabled={false}
-          text={'시작하기'}
+          text={'홈피서지 만들기'}
           onClick={saveNickname}
         />
       </Main>
@@ -39,17 +65,44 @@ const Intro = () => {
         fill={'weak'}
         size={'full'}
         disabled={false}
-        text={'홈피 둘러보기'}
+        text={'홈피서지  둘러보기'}
         onClick={lookAround}
       />
+      <TeamWrapper>
+        <Team>부 서 원</Team>
+        <Member>김동호, 박현재, 유나은, 유재민, 최주은</Member>
+      </TeamWrapper>
     </IntroWrapper>
   );
 };
 
+const TeamWrapper = styled.div`
+  margin-top: 70px;
+`;
+
+const Team = styled.div`
+  color: #f96f99;
+  text-align: center;
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 140%; /* 16.8px */
+`;
+
+const Member = styled.div`
+  text-align: center;
+
+  color: #a3a3a3;
+  font-size: 10px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 140%; /* 14px */
+`;
+
 const Logo = styled.img`
-  margin-top: 200px;
+  margin-top: 66px;
   width: 137px;
-  margin-bottom: 27px;
+  margin-bottom: 70px;
 `;
 
 const IntroWrapper = styled.div`
